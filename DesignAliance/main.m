1d65f899df1f143859ba49a3931da9fb284f6630
greatzues <zly668@foxmail.com>
2017-04-22 19:42:02 -0700

    Initial Commit

---
+++ DesignAliance/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DesignAliance
+//
+//  Created by zues on 17/4/22.
+//  Copyright © 2017年 zues. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
