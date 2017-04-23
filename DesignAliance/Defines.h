50c05ed1020c67d19fd853afe03b96fce7d1a662
greatzues <zly668@foxmail.com>
2017-04-22 20:43:13 -0700

    初始化项目，整理目录

---
+++ DesignAliance/Defines.h
@@ -0,0 +1,8 @@
+//
+//  Defines.h
+//  DesignAliance
+//
+//  Created by zues on 17/4/22.
+//  Copyright © 2017年 zues. All rights reserved.
+//
+
--- DesignAliance/HomePage.m
+++ DesignAliance/HomePage.m
@@ -6,23 +6,17 @@
 //  Copyright © 2017年 zues. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "HomePage.h"
 
-@interface ViewController ()
-
-@end
-
-@implementation ViewController
+@implementation HomePage
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
