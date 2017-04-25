4960fc01a31007d8c70e17a560a0abe9b0faa7ec
greatzues <zly668@foxmail.com>
2017-04-25 04:07:10 -0700

    添加父类控制器

--- DesignAliance/Controller/DABaseController.h
+++ DesignAliance/Controller/DABaseController.h
@@ -7,7 +7,24 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "DABaseOperation.h"
+#import "DAActivity.h"
 
-@interface DABaseController : UIViewController
+//设置顶部导航栏的动作和图片
+@interface DABaseController : UIViewController <DAOperationDelegate> {
+    DABaseOperation     *_operation;
+    DAActivity          *_activity;
+}
+
+- (void)showIndicator:(NSString *)tipMessage
+             autoHide:(BOOL)hide
+           afterDelay:(BOOL)delay;
+- (void)hideIndicator;
+
+- (void)setNavigationTitleImage:(NSString *)imageName;
+- (void)setNavigationLeft:(NSString *)imageName sel:(SEL)sel;
+- (void)setNavigationRight:(NSString *)imageName;
+- (void)setStatusBarStyle:(UIStatusBarStyle)style;
+- (IBAction)doBack:(id)sender;
 
 @end
